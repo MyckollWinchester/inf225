d12990d6f5c55e506dd3976f8b701f2a668b4ef0
MyckollWinchester <85040455+MyckollWinchester@users.noreply.github.com>
2024-03-17 19:51:51 -0300

    Applied Python code standarization with Ruff

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, redirect, url_for
+from werkzeug.wrappers.response import Response
+
 from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
-
 @app.errorhandler(404)
-def not_found(e):
+def not_found(_error: Exception) -> Response:
     return redirect(url_for("views.not_found"))
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=3030)
--- views.py
+++ views.py
@@ -1,16 +1,11 @@
-from flask import (
-    Blueprint,
-    render_template,
-)
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 @views.get("/")
-def index():
+def index() -> str:
     return render_template("index.html")
 
-
 @views.get("/404")
-def not_found():
+def not_found() -> str:
     return "404"
-
