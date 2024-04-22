4ab633aa6958c09e41fbea4be5b3a8c94bda6093
MyckollWinchester <85040455+MyckollWinchester@users.noreply.github.com>
2024-04-21 23:53:15 -0400

    Some hito 2 js thingies :3

---
+++ static/js/marcadores.js
@@ -0,0 +1,81 @@
+const talleristasPh = document.getElementById('talleristas-ph')
+
+const talleristas = fetch('http://localhost:8000/talleristas')
+
+talleristas.then(response => {
+  return response.json()
+}).then(data => {
+
+  if (data.length > 0) {
+    talleristasPh.innerHTML = ''
+    talleristasPh.classList.remove('note')
+    talleristasPh.style.display = 'flex'
+    talleristasPh.style.flexDirection = 'column'
+    talleristasPh.style.gap = '16px'
+
+    let i = 0
+
+    data.forEach(tallerista => {
+      i++
+      const card = document.createElement('div')
+      card.classList.add('card')
+
+      const image = document.createElement('img')
+      image.src = tallerista.foto_perfil
+      image.classList.add('card__image')
+
+      const div = document.createElement('div')
+
+      const title = document.createElement('h1')
+      title.classList.add('card__title')
+      title.innerHTML = tallerista.titulo
+
+      const ul = document.createElement('ul')
+
+      const li = document.createElement('li')
+      li.classList.add('card__contact')
+
+      li.innerHTML = `&#10095;<a href="${tallerista.enlace}" class="card__linkedin">${tallerista.enlace}</a>
+      <a id="verificar-${i}" href="" class="button button--green card__button card__button--${tallerista.verificado}">${tallerista.verificado ? "Verificado" : "No verificado" }</a>`
+
+      ul.appendChild(li)
+      div.appendChild(title)
+      div.appendChild(ul)
+      card.appendChild(image)
+      card.appendChild(div)
+      console.log(card)
+      talleristasPh.appendChild(card)
+    })
+
+    for (let i = 1; i <= data.length; i++) {
+      const button = document.getElementById(`verificar-${i}`)
+      button.addEventListener('click', e => {
+        verificarTallerista(e, i, data[i - 1])
+      })
+    }
+  }
+})
+
+const verificarTallerista = (e, i, data) => {
+  e.preventDefault()
+  fetch('http://localhost:8000/verificar-tallerista', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+
+  verificado = data.verificado ? false : true
+  const button = document.getElementById(`verificar-${i}`)
+
+  button.innerHTML = verificado ? "Verificado" : "No verificado"
+  button.classList.remove(`card__button--${data.verificado}`)
+  button.classList.add(`card__button--${verificado}`)
+}
---
+++ static/js/talleristas.js
@@ -0,0 +1,68 @@
+const searchInput = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
+const searchForm = document.getElementById('search-form-insumo')
+
+async function getTalleristas() {
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  return [
+    { id: 1, name: 'Talleristas 1' },
+    { id: 2, name: 'Talleristas 2' },
+    { id: 3, name: 'Talleristas 3' },
+    { id: 4, name: 'Talleristas 4' },
+    { id: 5, name: 'Talleristas 5' },
+    { id: 6, name: 'Talleristas 6' },
+    { id: 7, name: 'Talleristas 7' },
+    { id: 8, name: 'Talleristas 8' },
+    { id: 9, name: 'Talleristas 9' },
+    { id: 10, name: 'Talleristas 10' },
+  ]
+}
+
+searchForm.addEventListener('submit', async e => {
+  e.preventDefault()
+  if (searchInput.value.trim() === '') return
+
+  searchButton.innerHTML = `
+    <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none">
+      <path d="M12 3V6M12 18V21M6 12H3M21 12H18M5.63672 5.63672L7.75977 7.75977M16.2422 16.2422L18.3633 18.3633M18.3652 5.63477L16.2441 7.75586M7.75781 16.2422L5.63477 18.3652" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    Buscar
+  `
+
+  console.log(searchButton.children[0])
+  searchButton.children[0].classList.add('rotating')
+  searchButton.style.opacity = 0.75
+
+  searchButton.setAttribute('disabled', '')
+
+  searchButton.style.cursor = 'default'
+  
+  await getTalleristas().then(talleristas => {
+    console.log(talleristas)
+  })
+
+  searchButton.style.opacity = 1
+  searchButton.children[0].classList.remove('rotating')
+  searchButton.innerHTML = `
+    <svg width="1rem" height="1rem" fill="currentColor" viewBox="0 0 256 256">
+      <path d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"></path>
+    </svg>
+    Buscar
+  `
+
+  searchButton.style.cursor = 'pointer'
+
+  searchButton.removeAttribute('disabled')
+
+  console.log(searchInput.value)
+})
+
+searchInput.addEventListener('input', e => {
+  console.log(e.target.value)
+  if (searchInput.value.trim() === '') {
+    searchButton.setAttribute('disabled', '')
+  } else {
+    searchButton.removeAttribute('disabled')
+  }
+})
