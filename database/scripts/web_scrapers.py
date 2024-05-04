e55e2179ca15739a7b4ae02475216c662f135955
MyckollWinchester <85040455+MyckollWinchester@users.noreply.github.com>
2024-05-04 16:44:58 -0400

    Renamed and some code standarization

--- database/scripts/web_scrapers.py
+++ database/scripts/web_scrapers.py
@@ -1,21 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
 import json
 from concurrent.futures import ThreadPoolExecutor
 
+import requests
+from bs4 import BeautifulSoup
+
+
 class Producto:
-    def __init__(self, nombre, precio, enlace, imagen):
+    def __init__(self, nombre: str, precio: int, enlace: str, imagen: str) -> None:
         self.nombre = nombre
         self.precio = precio
         self.enlace = enlace
         self.imagen = imagen
 
-
 productos: list[Producto] = []
 
-def process_url(url):
+def process_url(url: str) -> None:
     global productos
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     html_content = response.text
     soup = BeautifulSoup(html_content, "html.parser")
     product_name = soup.find("meta", {"property": "og:title"})["content"]
@@ -25,10 +26,10 @@ def process_url(url):
     product_image_url = soup.find("meta", {"property": "og:image"})["content"]
     productos.append(Producto(product_name, product_price, url, product_image_url))
 
-def jumbo_search(query):
+def jumbo_search(query: str) -> list[dict]:
     global productos
     url = f"https://www.jumbo.cl/busqueda?ft={query}"
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     html_content = response.text
     soup = BeautifulSoup(html_content, "html.parser")
     json_scripts = soup.find_all("script", {"type": "application/ld+json"})
@@ -40,18 +41,16 @@ def jumbo_search(query):
                 urls = [item["url"] for item in json_data["itemListElement"]]
         except json.JSONDecodeError:
             pass
-    if not urls: return None
-
+    if not urls:
+        return None
     with ThreadPoolExecutor() as executor:
         executor.map(process_url, urls)
-    productos_tmp = []
-    for producto in productos:
-        productos_tmp.append({
-            "nombre": producto.nombre,
-            "precio": producto.precio,
-            "enlace": producto.enlace,
-            "imagen": producto.imagen
-        })
+    productos_tmp = [{
+        "nombre": producto.nombre,
+        "precio": producto.precio,
+        "enlace": producto.enlace,
+        "imagen": producto.imagen
+    } for producto in productos]
     productos = []
     return productos_tmp
 
